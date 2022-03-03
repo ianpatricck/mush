93400057d0636da525ffc4d1e9597dda5475c1d0
ianpatricck <ianpatrick499@gmail.com>
2022-03-03 19:01:19 -0300

    cleaned content

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-from config import config
-
-from os import environ
 import sys
+from usecases import Printer, Listener, Query
 
-environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-
-from pygame import mixer
-
-print("""
-░█▀▀░█▀█░█▀█░█░█░█▀▀░█▀▀░█▀█░█▀█░█▀▀░█▀▀
-░▀▀█░█░█░█▀█░█▀▄░█▀▀░▀▀█░█░█░█░█░█░█░▀▀█
-░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀▀
-""")
-
-print("Type 'pause' to pause, 'resume' to resume")
-print("Type 'stop' to stop")
-print("Type 'help' to show options")
-print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-
-if len(sys.argv) < 2:
-    mixer.init()
-    mixer.music.load(config["songs"] + config["initialSong"])
-    mixer.music.play()
-
-    print("\nListening {}\n".format(config["initialSong"]))
+Printer.main()
+Listener.initialPlay() if len(sys.argv) < 2 else None
 
 while True:
 
-    query = input("=> ")
-    query = query.lower()
+    args = input("=> ").lower()
 
-    if query == "pause":
-        print("[-] was paused")
-        mixer.music.pause()
-    
-    elif query == "resume":
-        print("[-] listening")
-        mixer.music.unpause()
-    
-    elif query == "stop":
-        print("[-] stopped, goodbye :D")
-        mixer.music.stop()
+    query = Query(args) 
+    query.run()
+
+    if (query.exitStatus != 0):
+        print("\nGood bye :D\n")
         break
-
