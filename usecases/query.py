2176defa4c43d0e4ba4fff59e9c5440a0ccbd0a4
ianpatricck <ianpatrick499@gmail.com>
2022-03-05 11:36:49 -0300

    clear method

--- usecases/query.py
+++ usecases/query.py
@@ -1,46 +1,14 @@
+import platform
+import os
+
 from .mixer import mixer
 from .listener import Listener
 
 class Query(Listener):
-
-    exitStatus = 0
-    
-    def __init__(self, arg):
-        self.query = arg
-
-    def run(self):
-        
-        if self.query == "help":
-            self.help()
-
-        elif self.query == "play":
-            self.play()
-
-        elif self.query == "pause":
-            self.pause()
-
-        elif self.query == "resume":
-            self.resume()
-
-        elif self.query == "stop":
-            self.stop()
-
-        elif self.query == "exit":
-            self.exitStatus += 1
-
-    def help(self):
-        print("""
-
-        Commands:
-
-        - set [path]             | set a playlist folder
-        - play                   | just play initial song
-        - play [song]            | play a chosen song
-        """)
-
+ 
     def play(self, song = None):
         if (song == None):
-            self.initialPlay()           
+            self.initialPlay()  
 
     def pause(self):
         print("\n[-] was paused\n")
@@ -53,4 +21,12 @@ class Query(Listener):
     def stop(self):
         print("\n[-] stopped\n")
         mixer.music.stop()
-    
+
+    def clear(self):
+        
+        if platform.system() == "Windows":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+        Printer.main()
