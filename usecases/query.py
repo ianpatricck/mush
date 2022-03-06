bf1e4c6e2b213ffc74a59f20d2b40758ea2ba9a6
ianpatricck <ianpatrick499@gmail.com>
2022-03-05 22:42:35 -0300

    clear and listSongs methods

--- usecases/query.py
+++ usecases/query.py
@@ -4,30 +4,36 @@ import os
 from .mixer import mixer
 from .listener import Listener
 from .printer import Printer
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 class Query(Listener):
  
     def play(self, song = None):
-        if (song == None):
-            self.initialPlay()  
+        self.initialPlay() if (song == None or not song or "") else self.playSong(song)
 
     def pause(self):
-        print("\n[-] was paused\n")
+        print("\n[+] was paused\n")
         mixer.music.pause()
     
     def resume(self):
-        print("\n[-] listening\n")
+        print("\n[+] listening\n")
         mixer.music.unpause()
     
     def stop(self):
-        print("\n[-] stopped\n")
+        print("\n[+] stopped\n")
         mixer.music.stop()
 
-    def clear(self):
-        
-        if platform.system() == "Windows":
-            os.system("cls")
-        else:
-            os.system("clear")
+    def clear(self): 
+        os.system("cls") if platform.system() == "Windows" else os.system("clear")
 
         Printer.main()
+
+    def listSongs(self):
+        songs = os.listdir(config["songs"])
+
+        for index, song in enumerate(songs, start=1):
+            print(index, song)
+        print("\n")
+
