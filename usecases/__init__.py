dc8f2c2a75ae1c2fd688ca2e3d826250f35547ad
ianpatricck <ianpatrick499@gmail.com>
2022-04-08 09:54:40 -0300

    Deleted usecases

--- usecases/__init__.py
+++
@@ -1,4 +0,0 @@
-from .printer import Printer
-from .listener import Listener
-from .actions import Actions
-from .run import Run
--- usecases/mixer/__init__.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-
-from pygame import mixer
-
-settings = "settings.json"
